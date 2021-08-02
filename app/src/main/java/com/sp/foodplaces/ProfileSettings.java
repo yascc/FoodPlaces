a5a25e57a1dbc757e543c926c77432362832de0e
yascc <yasccm@gmail.com>
2021-08-02 22:21:00 +0800

    Duplicate Profile to ProfileSettings

---
+++ app/src/main/java/com/sp/foodplaces/ProfileSettings.java
@@ -0,0 +1,98 @@
+package com.sp.foodplaces;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.slider.Slider;
+
+public class ProfileSettings extends AppCompatActivity {
+
+    private boolean mIsEnabled;
+    private GeofenceHelper mGeofencing;
+    Slider slider;
+    private static final String TAG = "Profile";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        this.setTitle("Profile Settings");
+
+
+        Switch onOffSwitch = (Switch) findViewById(R.id.enable_switch);
+        mIsEnabled = getPreferences(MODE_PRIVATE).getBoolean(getString(R.string.setting_enabled), false);
+        onOffSwitch.setChecked(mIsEnabled);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+                editor.putBoolean(getString(R.string.setting_enabled), isChecked);
+                mIsEnabled = isChecked;
+                editor.commit();
+                if (isChecked) GeofenceHelper.registerAllGeofences();
+                else GeofenceHelper.unRegisterAllGeofences();
+            }
+
+        });
+
+        slider = findViewById(R.id.radiusSlider);
+        slider.addOnChangeListener(new Slider.OnChangeListener() {
+            float radiusSliderValue = 80;
+            @Override
+            public void onValueChange(Slider slider, float value, boolean fromUser) {
+                radiusSliderValue = value;
+                if (radiusSliderValue!=0) GeofenceHelper.setGeofenceRadius(radiusSliderValue);
+                Log.d(TAG, "onValueChange: slider: " + radiusSliderValue);
+            }
+        });
+
+
+        //Initialize and Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.discovery:
+                        startActivity(new Intent(getApplicationContext(),
+                                MapsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.directory:
+                        startActivity(new Intent(getApplicationContext(),
+                                Directory.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.favorite:
+                        startActivity(new Intent(getApplicationContext(),
+                                Favorite.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile:
+                        return true;
+                    case R.id.notification:
+                        startActivity(new Intent(getApplicationContext(),
+                                Notification.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+}
