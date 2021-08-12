7b703b19517d87c6fb0a2f885fe77859b6f1242d
blur2sandy <blur2sandy@hotmail.com>
2021-08-12 14:11:32 +0800

    updated profile page

--- app/src/main/java/com/sp/foodplaces/Profile.java
+++ app/src/main/java/com/sp/foodplaces/Profile.java
@@ -69,7 +69,7 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),Login.class));
-                //finish();
+                finish();
             }
         });
 
